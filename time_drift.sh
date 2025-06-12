b38df78d7d51002506be39468442a56506bae611
Soheila Jafari <sjafarikhouzani@unm.edu>
2025-06-11 21:28:36 -0600

    new edited

--- time_drift.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for pod in $(kubectl get pods -n kafkastreamingdata -o name); do
-  (
-    echo -n "$pod: "
-    kubectl exec -n kafkastreamingdata   ${pod#pod/} -- date +%s.%N) &
-done
-
-wait
-
--- time_drift2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-for pod in $(kubectl get pods -n kafkastreamingdata -o name); do
-  local_time=$(date +%s.%N)
-  pod_time=$(kubectl exec -n kafkastreamingdata ${pod#pod/} -- date +%s.%N)
-  echo "$pod - local: $local_time | pod: $pod_time | drift: $(echo "$pod_time - $local_time" | bc)"
-done
-
